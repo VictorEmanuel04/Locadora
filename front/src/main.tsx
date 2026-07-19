1fd86b5d91a90416e8081ae0a5fd30f4b1b0871e
Víctor Emanuel <victoremanuel2235@gmail.com>
2026-07-18 22:33:32 -0300

    chore: apenas alterado título da página

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,9 @@
-// Exemplo de como deve estar o seu main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import App from './App'
 import { ErrorBoundary } from './components/ErrorBoundary'
-import { AuthProvider } from './context/AuthContext' // Atenção ao nome da pasta aqui!
+import { AuthProvider } from './context/AuthContext' 
 import { muiTheme } from './styles/theme'
 import './index.css'
 
